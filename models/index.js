e188e2293c4fda2b574d645330a8bf13dbaeaa3c
Christopher Fujino <christopherfujino@gmail.com>
2017-07-20 13:39:54 -0700

    fixed: requiring of album.js now explicit

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/tunely')
 
-let Album = require('album.js')
+let Album = require('./album.js')
 
 module.exports = {
   Album: Album
