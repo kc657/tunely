c26abd10ac3b8fbe40e5c7a0f186d28640ac688c
kc657 <kvn.0218@gmail.com>
2017-07-21 16:02:04 -0700

    Formatting Changed

--- controllers/albumSongsController.js
+++ controllers/albumSongsController.js
@@ -2,12 +2,6 @@ const db = require('../models')
 const bodyParser = require('body-parser')
 
 function create (req, res) {
-  let album_id = req.params.album_id
-  db.Album.findById(album_id, function (err, album){
-    if (err) return res.status(500).json(err)
-    if (album === null) return res.status(404).json({message: "didn't find album"})
-    console.log("album found");
-  })
   let newSong = req.body
   db.Album.create(newSong, function (err, song) {
     if (err) return res.status(500).json(err)
